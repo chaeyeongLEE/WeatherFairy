d39e1cdf36ea807183b2659606b93ee81726f77a
chaeyeongLEE <dh503401@naver.com>
2023-01-30 13:09:28 +0900

    feat: main delete

--- project/src/components/Header.jsx
+++ project/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/Header.css';
 
 export default function Header() {
   return (
@@ -7,8 +8,9 @@ export default function Header() {
       <nav>
         <ul style={{ display: 'flex', justifyContent: 'space-around' }}>
           <li>
-            <Link to="/weather">날씨창 이동</Link>
+            <Link to="/">메인 홈 이동</Link>
           </li>
+
           <li>
             <Link to="/board">날씨다이어리 이동</Link>
           </li>
