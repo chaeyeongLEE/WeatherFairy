e428a1e6d890a5275f65edf01b1a4daf69088fa7
chaeyeongLEE <dh503401@naver.com>
2023-01-31 14:54:51 +0900

    feat: icon plus

--- project/src/components/Header.jsx
+++ project/src/components/Header.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Header.css';
+import homeImg from '../home.png';
+import diaryImg from '../diary.png';
 
 export default function Header() {
+  const imgStyle = { width: '40px' };
   return (
     <>
       <nav>
         <ul style={{ display: 'flex', justifyContent: 'space-around' }}>
           <li>
-            <Link to="/">메인 홈 이동</Link>
+            <Link to="/">
+              {' '}
+              <img src={homeImg} alt="홈" style={imgStyle} />{' '}
+            </Link>
           </li>
 
           <li>
-            <Link to="/board">날씨다이어리 이동</Link>
+            <Link to="/board">
+              {' '}
+              <img src={diaryImg} alt="다이어리" style={imgStyle} />{' '}
+            </Link>
           </li>
         </ul>
       </nav>
