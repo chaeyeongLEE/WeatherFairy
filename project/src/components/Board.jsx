e7e83e309b5bb9e1c577c9e77e1acca7a9efd95e
leechaeyeon321 <leechaeyeon321@gmail.com>
2023-02-05 20:47:28 +0900

    feat: board little edit

--- project/src/components/Board.jsx
+++ project/src/components/Board.jsx
@@ -101,16 +101,16 @@ export default function Board() {
     let months = [
       '1',
       '2',
-      'Mar',
-      'Apr',
-      'May',
-      'Jun',
-      'Jul',
-      'Aug',
-      'Sep',
-      'Oct',
-      'Nov',
-      'Dec',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      '10',
+      '11',
+      '12',
     ];
 
     let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Tur', 'Fri', 'Sat'];
